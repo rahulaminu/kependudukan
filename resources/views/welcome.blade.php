f27c9702c51a804fd0aaf5277363408baf73cc44
Regina Nata Bombang <gyurinb15@gmail.com>
2024-11-12 16:06:31 +0800

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,48 @@
-@extends('layout.menu')
-	@section('konten')
-		<h3>Halo! ini halaman kependudukan</h3>
-	@endsection
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
+    <link href="{{ asset('assetss/styles.css') }}" rel="stylesheet">
+    <title>Web Design Mastery | Camp</title>
+  </head>
+  <body>
+    <nav>
+      <div class="nav__header">
+        <div class="nav__logo">
+          <a href="index.html">Neumedira</a>
+        </div>
+        <div class="nav__menu__btn" id="menu-btn">
+          <i class="ri-menu-line"></i>
+        </div>
+      </div>
+      <ul class="nav__links" id="nav-links">
+        <li><a href="index.html">HOME</a></li>
+        <li><a href="#">DATA KEPENDUDUKAN</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </nav>
+    <div class="container">
+      <div class="container__left">
+        <h1>Kependudukan Indonesia</h1>
+        <div class="container__btn">
+          <button class="btn">TAMBAH DATA</button>
+        </div>
+      </div>
+      <div class="container__right">
+        <div class="content">
+        </div>
+      </div>
+      <div class="message">
+        
+      </div>
+    </div>
+
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="main.js"></script>
+  </body>
+</html>
